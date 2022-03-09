47fcb6c3bb796d107fa533aa565b98cf55d70ede
marc2k3 <marc2003@live.co.uk>
2022-03-09 14:35:42 +0000

    Cover resizer: bump version + fix typo

--- foo_cover_resizer/CoverResizer.hpp
+++ foo_cover_resizer/CoverResizer.hpp
@@ -5,7 +5,7 @@ using resizer::Format;
 class CoverResizer : public threaded_process_callback
 {
 public:
-	CoverResizer(metadb_handle_list_cref handles, resizer::Format format, bool convert_only = false);
+	CoverResizer(metadb_handle_list_cref handles, Format format, bool convert_only = false);
 
 	void run(threaded_process_status& status, abort_callback& abort) override;
 
